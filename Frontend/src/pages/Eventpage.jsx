cb03d8ca02e66d47f44a8347b83d6c9ea3cd46fa
DevaKrishna07 <devakrishna1418@gmail.com>
2025-07-25 19:53:24 +0530

    initialized Event page

--- Frontend/src/pages/Eventpage.jsx
+++ Frontend/src/pages/Eventpage.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import LinedText, { SideLinedText } from '../components/LinedText'
 
 function Eventpage() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="w-screen min-h-screen bg-gradient-to-r from-black to-gray-900 flex flex-col items-center justify-start pt-4">
+            <div className="w-[90%] backdrop-blur-md bg-white/10 border border-white/10 shadow-lg rounded-md p-2 flex flex-col items-center justify-start">
+                <div className="w-[50%] aspect-square bg-gradient-to-r from-black to-gray-900 flex items-center justify-center rounded-xl overflow-hidden">
+                    <h1 className='text-white font-bold'>heelo</h1>
+                </div>
+            
+            <div className="w-full flex items-center justify-start py-4 ">
+                <LinedText text={"Event Details"} />
+            </div>
+            </div>
+        </div>
+    )
 }
 
 export default Eventpage

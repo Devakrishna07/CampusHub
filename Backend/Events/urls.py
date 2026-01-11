6cd96584860a317106ff8cd98fcb82c38ea69f79
DevaKrishna07 <devakrishna1418@gmail.com>
2026-01-11 12:26:18 +0530

    initialized repo

--- Backend/Events/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import PublicEventsView, EventCreateView, EventEditView
-
-urlpatterns = [
-    path("public/", PublicEventsView.as_view(), name="Public_events_view"),
-    path("create/", EventCreateView.as_view(), name="EventCreateView"),
-    path("edit/", EventEditView.as_view(), name="EventEditView"),
-]
-
--- Backend/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import RegisterView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register_user"),
-    path("token/", TokenObtainPairView.as_view(), name="Token_obtain/login"),
-    path("refresh_token/", TokenRefreshView.as_view(), name="Token_refresh")
-]
